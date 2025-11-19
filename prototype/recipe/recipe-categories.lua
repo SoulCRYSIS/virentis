7231acdf6973f3e1f82f4cb701823333307ce95b
Phurin Taengsriwan <phurin.taengsriwan@apartechnologies.com>
2025-11-19 16:41:08 +0700

    base graphic: coneflora, gazeberry

--- prototype/recipe/recipe-categories.lua
+++ prototype/recipe/recipe-categories.lua
@@ -8,8 +8,22 @@ data:extend({
   ---@type data.RecipeCategory
   {
     type = "recipe-category",
-    name = "trading",
+    name = "trading-t1",
     subgroup = "virentis-trades",
-    order = "a"
+    order = "ka"
+  },
+  ---@type data.RecipeCategory
+  {
+    type = "recipe-category",
+    name = "trading-t2",
+    subgroup = "virentis-trades",
+    order = "kb"
+  },
+  ---@type data.RecipeCategory
+  {
+    type = "recipe-category",
+    name = "trading-t3",
+    subgroup = "virentis-trades",
+    order = "kc"
   },
 })
--- prototype/recipe/trades.lua
+++ prototype/recipe/trades.lua
@@ -1,11 +1,35 @@
+local function trade_icon(from, to, size)
+  ---@type data.IconData[]
+  return {
+    {
+      icon = "__virentis__/graphic/icon/" .. from .. ".png",
+      icon_size = size or 256,
+      scale = 0.7,
+      shift = { -48, -48 },
+    },
+    {
+      icon = "__virentis__/graphic/icon/" .. to .. ".png",
+      icon_size = size or 256,
+      scale = 0.7,
+      shift = { 48, 48 },
+    },
+    {
+      icon = "__virentis__/graphic/icon/trade-arrow.png",
+      icon_size = 256,
+      scale = 0.7,
+    },
+  }
+end
+
 data:extend({
   ---@type data.RecipePrototype
   {
     type = "recipe",
     name = "trade-gnarpod-fruit-to-iron-plate",
     subgroup = "virentis-trades",
-    category = "trading",
+    category = "trading-t1",
     order = "a",
+    icon = trade_icon("gnarpod-fruit", "iron-plate"),
     ingredients = {
       { type = "item", name = "gnarpod-fruit", amount = 1 },
     },
