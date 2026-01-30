c0e42024d386e2bfd61b6dcb0452f44eef4544cf
Phurin Taengsriwan <phurin.taengsriwan@apartechnologies.com>
2026-01-30 13:07:34 +0700

    revert map gen change

--- prototypes/noise-expressions/enemies.lua
+++ prototypes/noise-expressions/enemies.lua
@@ -14,12 +14,12 @@ data:extend({
     type = "noise-expression",
     name = "virentis_pentapod_spawner",
     expression =
-    "max(0.01 * virentis_starting_enemies, max(min(0.02, enemy_autoplace_base(0, 8)), min(0.001, virentis_fertile_spots_coastal_default * 5000 - virentis_biome_mask_green * 25000)) * (distance > 500 * virentis_starting_area_multiplier)) * virentis_above_deep_water_mask"
+    "max(0.01 * virentis_starting_enemies, max(min(0.02, enemy_autoplace_base(0, 8)), min(0.001, virentis_fertile_spots_coastal * 5000 - virentis_biome_mask_green * 25000)) * (distance > 500 * virentis_starting_area_multiplier)) * virentis_above_deep_water_mask"
   },
   {
     type = "noise-expression",
     name = "virentis_pentapod_spawner_small",
     expression =
-    "max(0.02 * virentis_starting_enemies, 0.02 * virentis_starting_enemies_safe, min(0.02, enemy_autoplace_base(0, 8)), min(0.001, virentis_fertile_spots_coastal_default * 5000 - virentis_biome_mask_green * 25000)) * virentis_above_deep_water_mask"
+    "max(0.02 * virentis_starting_enemies, 0.02 * virentis_starting_enemies_safe, min(0.02, enemy_autoplace_base(0, 8)), min(0.001, virentis_fertile_spots_coastal * 5000 - virentis_biome_mask_green * 25000)) * virentis_above_deep_water_mask"
   },
 })
--- prototypes/noise-expressions/rockpools.lua
+++ prototypes/noise-expressions/rockpools.lua
@@ -4,7 +4,7 @@ data:extend(
     type = "noise-expression",
     name = "virentis_rockpools",
     expression = "clamp(1 - 2 * abs(multioctave_noise{x = x, y = y, persistence = 0.75, seed0 = map_seed, seed1 = 1000000, octaves = 4, input_scale = 1/45}), 0, 1)\z
-                  * virentis_select(rockpools_mask_noise + min(0, distance / 150 - 2) - max(0, virentis_fertile_spots_coastal_default), 1.4, 3, 0.4, 0, 1) * (1 + (rockpools_mask_noise - 1) / 10)",
+                  * virentis_select(rockpools_mask_noise + min(0, distance / 150 - 2) - max(0, virentis_fertile_spots_coastal_raw_default), 1.4, 3, 0.4, 0, 1) * (1 + (rockpools_mask_noise - 1) / 10)",
     local_expressions = {
       rockpools_mask_noise = "abs(multioctave_noise{x = x, y = y, persistence = 0.7, seed0 = map_seed, seed1 = 1000000, octaves = 3, input_scale = 1/75})"
     }
